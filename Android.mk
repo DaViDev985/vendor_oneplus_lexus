e57d0fef4ac35c7673016016bb9eb38a7945fb37
grep <grepfox@tutamail.com>
2025-12-30 19:55:21 +0000

    lexus: Initial import proprietary files from CPH2707_16.0.1.300(EX01)
    Change-Id: Ia7ae4ca148973f2116d34f37daec334ea46a09af

---
+++ Android.mk
@@ -0,0 +1,35 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lexus)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,9e8faba1a7380a1638742126da13c9008e0e9b40)
+$(call add-radio-file-sha1-checked,radio/aop.img,3c1437505944dec873130ea40238144aaaaef0ae)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,266fe88e4fe8d7b7f44df3084ea68e4888690020)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,34513c98cc88ed45851cfa0db401f4ce2d6bcd00)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,c167dea26bb01cec53508891a06e2aaa92959388)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,58f0097e1e5490d00b8f37377be92493309efa4a)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,314748d277d11abcee08a824df33725fbf0a00d7)
+$(call add-radio-file-sha1-checked,radio/dsp.img,7e81146e07bb4120616a084cc932dc4836420d29)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,f77f4cbc1e1225b2dc7bdd183ccc2bce242845ad)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,14a082e85ec14e5b338032113e0f3f82609c7dd9)
+$(call add-radio-file-sha1-checked,radio/hyp.img,4f5d5d023510e006d32321c301dfab6e75623fd6)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,16d875317ec60555a69951a61d86d005a75e896e)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,d0d75abd1bed5b9b599e5024737df6c90f2ae4d3)
+$(call add-radio-file-sha1-checked,radio/modem.img,eca81f6480a516cc010a7041912b7275a27855cf)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,d4cd54f828dbfb4fcdb4c81fd6f1900da0e89287)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,1f694d0228010f66a4feb5d307dbbe3d01b2bd68)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,167b37c779761eeff2832fe31591d4f1a2c7ba51)
+$(call add-radio-file-sha1-checked,radio/shrm.img,0b62578b977b8a11fee77bffd7efcaece8c1aace)
+$(call add-radio-file-sha1-checked,radio/splash.img,a62387b428702e55e659bfd3a5ce0c662fabf41a)
+$(call add-radio-file-sha1-checked,radio/tz.img,3e1de899906d1777f9a7e4e39f955795a29a0f5a)
+$(call add-radio-file-sha1-checked,radio/uefi.img,6658f542cbfd438c69be1f378f75c74f9b966487)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,16a27506b5830e81859ba72c5cc41ba181da567f)
+$(call add-radio-file-sha1-checked,radio/xbl.img,ac5ba63db69ac0d792b08f96b17498b1961afc61)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,d3a76a34edba133681d23f7acbe02c042ac2b69c)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,a1902a7bc7af6553dd7e2d169f2f73d35588f3af)
+
+endif
