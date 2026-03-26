0cda0127c66e5bc7792772496cb921e197f46fb6
grepfox <grepfox@tutamail.com>
2026-03-26 19:45:38 +0000

    lexus: Update from OOS 11.C.20
    Change-Id: I38edf9c704cdb5c26c0cd0fe2a34df3b5f12797f

--- lexus-vendor.mk
+++ lexus-vendor.mk
@@ -1427,6 +1427,7 @@ PRODUCT_PACKAGES += \
     libdisplayskuutils \
     libdpp_manager \
     libdpps \
+    libdtlm \
     libdualcam_optical_zoom_control \
     libdualcam_video_optical_zoom \
     libeepromcutter \
